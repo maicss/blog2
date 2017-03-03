1975a8548a09bccce5bd8e67f3d7b2d89a08da25
maicss <maicss_mk@163.com>
2017-03-03 13:26:29 +0800

    去掉证书验证，再测试一下github

--- src/routers/github.js
+++ src/routers/github.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 const scanAndRender = require('../scanMD');
 module.exports = function (req, res, next) {
     fs.writeFileSync('./github-push-data', JSON.stringify(req.body));
+    console.log('========== github push hook ==========');
     console.log(req.body);
+    console.log('========== github push hook ==========');
     res.json(Object.assign({}, req.body.commits.added, req.body.commits.removed, req.body.commits.modified));
     // scanAndRender(function (r) {
     //     res.json(r);
--- src/scanMD.js
+++ src/scanMD.js
@@ -20,7 +20,7 @@ const getHash = require('./db-op').getPostsSha;
 const saveHash = require('./db-op').savePostsSha;
 const savePostInfo = require('./db-op').savePostInfo;
 const logger = require('./mongo-logger');
-const pull = require('./gitPull');
+// const pull = require('./gitPull');
 const renderer = require('./render');
 
 const MD_DIR = require('../env').MD_DIR;
@@ -28,11 +28,11 @@ const ALGORITHM = 'sha256';
 const fileNameRegExp = /[\u4e00-\u9fa5\w()（） -]+\.md/;
 
 
-let pullRes = new Promise(function (resolve, reject) {
-    pull(function (r) {
-        r ? resolve() : reject()
-    })
-});
+// let pullRes = new Promise(function (resolve, reject) {
+//     pull(function (r) {
+//         r ? resolve() : reject()
+//     })
+// });
 
 let readFileSha = new Promise(function (resolve, reject) {
     fs.readdir(path.resolve(__dirname, MD_DIR), function (err, files) {
@@ -79,70 +79,64 @@ let readDBSha = new Promise(function (resolve, reject) {
     })
 });
 
-module.exports = function(callback) {
-    pullRes.then(() => {
-        Promise.all([readDBSha, readFileSha]).then(function (res) {
-            let [{results: dbRes}, fileRes] = res;
-            let copy = [...fileRes];
-            for (let fileInfo of fileRes) {
-                for (let dbInfo of dbRes) {
-                    if (dbInfo.escapeName === fileInfo.escapeName && dbInfo.sha === fileInfo.sha) {
-                        copy.splice(copy.indexOf(fileInfo), 1);
-                        break;
-                    }
+module.exports = function (callback) {
+    Promise.all([readDBSha, readFileSha]).then(function (res) {
+        let [{results: dbRes}, fileRes] = res;
+        let copy = [...fileRes];
+        for (let fileInfo of fileRes) {
+            for (let dbInfo of dbRes) {
+                if (dbInfo.escapeName === fileInfo.escapeName && dbInfo.sha === fileInfo.sha) {
+                    copy.splice(copy.indexOf(fileInfo), 1);
+                    break;
                 }
             }
+        }
 
-            if (copy.length) {
-                saveHash(copy, function (d) {
-                    if (d.opResStr === 'success' && d.results.result.ok === 1) {
-                        // render md
-                        copy.forEach(info => {
-                            renderer(info, function (renderResult) {
-                                /*
-                                 * todo:
-                                 * date不存在，这个自己写的东西，出现的概率很小
-                                 * readCount 和 commentCount 只能在初始化的时候赋值，而前面的代码是只要修改文件就会调用这个函数。
-                                 * */
-                                let publicInfo = {
-                                    title: renderResult.title,
-                                    originalFileName: info.originalFileName,
-                                    escapeName: info.escapeName,
-                                    createDateStr: renderResult.date,
-                                    createDate: new Date(renderResult.date) * 1,
-                                    tags: renderResult.tags,
-                                    readCount: 0,
-                                };
-                                let postInfo = Object.assign({abstract: renderResult.abstract}, publicInfo);
+        if (copy.length) {
+            saveHash(copy, function (d) {
+                if (d.opResStr === 'success' && d.results.result.ok === 1) {
+                    // render md
+                    copy.forEach(info => {
+                        renderer(info, function (renderResult) {
+                            /*
+                             * todo:
+                             * date不存在，这个自己写的东西，出现的概率很小
+                             * readCount 和 commentCount 只能在初始化的时候赋值，而前面的代码是只要修改文件就会调用这个函数。
+                             * */
+                            let publicInfo = {
+                                title: renderResult.title,
+                                originalFileName: info.originalFileName,
+                                escapeName: info.escapeName,
+                                createDateStr: renderResult.date,
+                                createDate: new Date(renderResult.date) * 1,
+                                tags: renderResult.tags,
+                                readCount: 0,
+                            };
+                            let postInfo = Object.assign({abstract: renderResult.abstract}, publicInfo);
 
-                                savePostInfo(postInfo, function (d) {
-                                    if (d.opResStr === 'success') {
-                                        logger.info('scan and render and save post success.');
-                                        callback('prefect');
-                                    } else {
-                                        logger.error('save post error: ', d.error || d.fault);
-                                        callback('save db failed');
-                                    }
-                                });
+                            savePostInfo(postInfo, function (d) {
+                                if (d.opResStr === 'success') {
+                                    logger.info('scan and render and save post success.');
+                                    callback('prefect');
+                                } else {
+                                    logger.error('save post error: ', d.error || d.fault);
+                                    callback('save db failed');
+                                }
                             });
-
                         });
-                    } else {
-                        logger('scanMD module, save hash error: ', d);
-                        callback('save hash failed');
-                    }
-                })
-            }
 
-        }).catch(function (err) {
-            // 这个logger没必要
-            // logger.error('scanMD module, Promise all error: ', err);
-            console.log(err)
-        });
-    }).catch(e => {
-        // todo: retry
-        logger.error('pull error or failed: ', e);
-        callback('github pull failed');
+                    });
+                } else {
+                    logger('scanMD module, save hash error: ', d);
+                    callback('save hash failed');
+                }
+            })
+        }
+
+    }).catch(function (err) {
+        // 这个logger没必要
+        // logger.error('scanMD module, Promise all error: ', err);
+        console.log(err)
     });
 };
 
